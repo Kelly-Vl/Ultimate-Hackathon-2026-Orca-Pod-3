b671c8273be992431932aff5d2ccad58777c3de3
ZoeleneVelinsky <24374107@student.uwa.edu.au>
2026-08-01 11:55:10 +0800

    cleaned data for supply based on pod

---
+++ supply.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+supply_df = pd.read_csv(
+    "Matilda Bay/Matilday Bay/pod_supply_data/matilda_bay_pod_supply_data.csv"
+)
+
+supply_df_pod1 = supply_df[supply_df["pod_id"] == "Pod 1"]
+#print(supply_df_pod1)
+
+print("Pod 1:")
+print(supply_df_pod1[[
+    "event_id",
+    "event_date",
+    "pod_id",
+    "population",
+    "need_status",
+    "amount_requested",
+    "pool_available_that_resource",
+    "fair_priority_score",
+    "amount_allocated",
+    "unmet_amount"
+]].to_string(index=False))
+
+supply_df_pod2 = supply_df[supply_df["pod_id"] == "Pod 2"]
+print("\n Pod 2: ")
+print(supply_df_pod2[[
+    "event_id",
+    "event_date",
+    "pod_id",
+    "population",
+    "need_status",
+    "amount_requested",
+    "pool_available_that_resource",
+    "fair_priority_score",
+    "amount_allocated",
+    "unmet_amount"
+]].to_string(index=False))
+
+supply_df_pod4 = supply_df[supply_df["pod_id"] == "Pod 4"]
+print("\n Pod 4: ")
+print(supply_df_pod4[[
+    "event_id",
+    "event_date",
+    "pod_id",
+    "population",
+    "need_status",
+    "amount_requested",
+    "pool_available_that_resource",
+    "fair_priority_score",
+    "amount_allocated",
+    "unmet_amount"
+]].to_string(index=False))
