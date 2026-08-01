705c8c485bb918947b4f49047d31a41246dd2ff3
ZoeleneVelinsky <24374107@student.uwa.edu.au>
2026-08-01 12:00:57 +0800

    Put the cleaned file into csv

--- supply.py
+++ supply.py
@@ -6,8 +6,7 @@ supply_df = pd.read_csv(
 
 supply_df_pod1 = supply_df[supply_df["pod_id"] == "Pod 1"]
 #print(supply_df_pod1)
-
-print("Pod 1:")
+'''
 print(supply_df_pod1[[
     "event_id",
     "event_date",
@@ -20,33 +19,11 @@ print(supply_df_pod1[[
     "amount_allocated",
     "unmet_amount"
 ]].to_string(index=False))
+'''
+supply_df_pod1.to_csv("pod1_output.csv", index=False)
 
 supply_df_pod2 = supply_df[supply_df["pod_id"] == "Pod 2"]
-print("\n Pod 2: ")
-print(supply_df_pod2[[
-    "event_id",
-    "event_date",
-    "pod_id",
-    "population",
-    "need_status",
-    "amount_requested",
-    "pool_available_that_resource",
-    "fair_priority_score",
-    "amount_allocated",
-    "unmet_amount"
-]].to_string(index=False))
+supply_df_pod2.to_csv("pod2_output.csv", index=False)
 
 supply_df_pod4 = supply_df[supply_df["pod_id"] == "Pod 4"]
-print("\n Pod 4: ")
-print(supply_df_pod4[[
-    "event_id",
-    "event_date",
-    "pod_id",
-    "population",
-    "need_status",
-    "amount_requested",
-    "pool_available_that_resource",
-    "fair_priority_score",
-    "amount_allocated",
-    "unmet_amount"
-]].to_string(index=False))
+supply_df_pod4.to_csv("pod4_output.csv", index=False)
